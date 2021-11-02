41b2ba3d25ecfd863a985178887e1f57e67554ae
Michael T Lombardi <michael.t.lombardi@gmail.com>
2021-11-02 17:39:38 -0500

    (GH-18) Add docker backend status method
    Prior to this commit, the docker backend was scaffolded but unimplemented.
    
    This commit adds the new BackendStatus struct, which generically reports
    whether a particular backend is available and relevent information about
    that backend; in the case of an unavailable backend, it should report
    *why* that backend is unavailable (surfacing any error messages) and in
    the case of an available backend, useful information.
    
    This commit adds a new DockerClient interface to the docker backend in
    order to enable mocking/unit testing. The only function currently used
    from the client is the ServerVersion function, consumed in the new Status()
    function for the backend.
    
    The Status() function is implemented on the docker backend to call the
    ServerVersion() docker client function. If this call errors, then the
    docker service/backend is unavailable. The most likely reason for this
    is the Docker Engine not running, so special handling is added for that
    edge case to handle it in a slightly more friendly way for end users.
    
    If the ServerVersion() docker client function does *not* error it returns
    information about Docker which is useful to surface; in this case, the
    platform (Docker community/enterprise, desktop/server), the version, and
    the API version being used.
    
    Finally, this commit adds unit testing for the new Status() function and
    includes mocks for the Docker Client. In this PR the mocks are kept in the
    same file as the unit tests; if/when the mocks need to be used in another
    test file, it may make sense to move them into a mocks folder.
    
    Resolves #18.

--- pkg/prm/backend.go
+++ pkg/prm/backend.go
@@ -6,3 +6,11 @@ type BackendI interface {
 	Validate(tool *Tool) (ToolExitCode, error)
 	Exec(tool *Tool, args []string) (ToolExitCode, error)
 }
+
+// The BackendStatus must report whether the backend is available
+// and any useful status information; in the case of the backend
+// being unavailable, report the error message to the user.
+type BackendStatus struct {
+	IsAvailable bool
+	StatusMsg   string
+}
--- pkg/prm/backends/docker.go
+++ pkg/prm/backends/docker.go
@@ -1,8 +1,22 @@
 package backends
 
-import "github.com/puppetlabs/prm/pkg/prm"
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/docker/docker/api/types"
+	"github.com/puppetlabs/prm/pkg/prm"
+)
 
 type Docker struct {
+	// We need to be able to mock the docker client in testing
+	Client DockerClient
+}
+
+type DockerClient interface {
+	// All docker client functions must be noted here so they can be mocked
+	ServerVersion(context.Context) (types.Version, error)
 }
 
 func (*Docker) GetTool(toolName string, prmConfig prm.Config) (prm.Tool, error) {
@@ -18,4 +32,34 @@ func (*Docker) Validate(tool *prm.Tool) (prm.ToolExitCode, error) {
 func (*Docker) Exec(tool *prm.Tool, args []string) (prm.ToolExitCode, error) {
 	// TODO
 	return prm.FAILURE, nil
+}
+
+// Check to see if the Docker runtime is available:
+// if so, return true and info about Docker on this node;
+// if not, return false and the error message
+func (d *Docker) Status() prm.BackendStatus {
+	// The client does not error on creation if the background service is not running,
+	// but attempting to list the containers does.
+	dockerInfo, err := d.Client.ServerVersion(context.Background())
+	if err != nil {
+		// message := fmt.Sprintf("%s", err)
+		message := err.Error()
+		// This is 90% likely the reason this command fails;
+		// the alternative error message is lengthy and includes
+		// references to pipes and the API which are more likely
+		// to confuse than help; so trim it to the most useful info.
+		daemonNotRunning := "error during connect: This error may indicate that the docker daemon is not running."
+		if strings.Contains(message, daemonNotRunning) {
+			message = daemonNotRunning
+		}
+		return prm.BackendStatus{
+			IsAvailable: false,
+			StatusMsg:   message,
+		}
+	}
+	status := fmt.Sprintf("\tPlatform: %s\n\tVersion: %s\n\tAPI Version: %s", dockerInfo.Platform.Name, dockerInfo.Version, dockerInfo.APIVersion)
+	return prm.BackendStatus{
+		IsAvailable: true,
+		StatusMsg:   status,
+	}
 }
