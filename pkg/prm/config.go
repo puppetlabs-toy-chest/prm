b71c0c7bd1cb2791546bb494ebfe3edb6f51bb4e
sanfrancrisko <ciaran.mccrisken@puppet.com>
2021-11-16 17:47:25 +0000

    (GH-18) Make default cfg values constants

--- pkg/prm/config.go
+++ pkg/prm/config.go
@@ -4,14 +4,17 @@ import (
 	"fmt"
 
 	"github.com/Masterminds/semver"
+	"github.com/rs/zerolog/log"
 	"github.com/spf13/viper"
 )
 
 const (
-	PuppetCmdFlag   string = "puppet"
-	BackendCmdFlag  string = "backend"
-	PuppetVerCfgKey string = "puppetversion" // Should match Config struct key.
-	BackendCfgKey   string = "backend"       // Should match Config struct key.
+	PuppetCmdFlag    string      = "puppet"
+	BackendCmdFlag   string      = "backend"
+	PuppetVerCfgKey  string      = "puppetversion" // Should match Config struct key.
+	BackendCfgKey    string      = "backend"       // Should match Config struct key.
+	DefaultPuppetVer string      = "7"
+	DefaultBackend   BackendType = DOCKER
 )
 
 type Config struct {
@@ -23,13 +26,15 @@ var RunningConfig Config
 
 func GenerateDefaultCfg() {
 	// Generate default configuration
-	puppetVer, err := semver.NewVersion("7")
+	puppetVer, err := semver.NewVersion(DefaultPuppetVer)
 	if err != nil {
 		panic(fmt.Sprintf("Unable to generate default cfg value for 'puppet': %s", err))
 	}
 
+	log.Trace().Msgf("Setting default config (%s: %s)", PuppetVerCfgKey, puppetVer.String())
 	viper.SetDefault(PuppetVerCfgKey, puppetVer)
-	viper.SetDefault(BackendCfgKey, DOCKER)
+	log.Trace().Msgf("Setting default config (%s: %s)", BackendCfgKey, DefaultBackend)
+	viper.SetDefault(BackendCfgKey, string(DefaultBackend))
 }
 
 func LoadConfig() error {
